40d6908a194d90c7025bd0cc084b9c114cc363aa
FazeNikola0212 <nikicha.041@gmail.com>
2025-04-12 10:06:26 +0300

    Декларира операции за управление на билети и събития.
    Интерфейсът определя методи за добавяне на събития, резервации, покупка, справки и проверка на билети в системата.

---
+++ src/Interfaces/TicketCommands.java
@@ -0,0 +1,12 @@
+package Interfaces;
+
+public interface TicketCommands {
+    void addEvent(String data);
+    void freeSeats(String data);
+    void book(String data);
+    void unbook(String data);
+    void buy(String data);
+    void bookings(String data);
+    void check(String data);
+    void report(String data);
+}
